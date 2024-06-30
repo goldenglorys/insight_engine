b8fe4fc3ca01cbd24d1c48d995f05c4122020b84
goldenglorys <olusolagloryolamide@gmail.com>
2024-06-30 21:32:27 +0300

    chore: update documentation

--- engine/embeddings.py
+++ engine/embeddings.py
@@ -47,8 +47,8 @@ class SentenceTransformerEmbeddings(Embeddings):
             texts (List[str]): A list of strings to embed.
 
         Returns:
-            List[List[float]]: A list of embeddings, each embedding being 
-            a list of floats.
+            List[List[float]]: A list of embeddings, 
+            each embedding being a list of floats.
         """
         embeddings = self.model.encode(texts)
         return embeddings.tolist()
